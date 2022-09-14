d36bf5a051a77a301bf3d9992bb11f7dcee0c42c
sotaheavymetal21 <sota_anegawa@pj100.biz>
2022-09-14 12:31:49 +0900

    generator100()を追加

--- yield.py
+++ yield.py
@@ -6,12 +6,22 @@ def generator2():
     yield "two"
 
 
-def generator(g1, g2):
+def generator3():
+    yield "three"
+
+
+def generator100():
+    yield "one hundred"
+
+
+def generator(g1, g2, g3, g4):
     yield from g1
     yield from g2
+    yield from g3
+    yield from g4
 
 
-gen = generator(generator1(), generator2())
+gen = generator(generator1(), generator2(), generator3(), generator100())
 
 for x in gen:
     print(x)
