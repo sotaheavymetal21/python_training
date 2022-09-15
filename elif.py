1fec7841c06a1fcd059af318a395be5f5aca832b
sotaheavymetal21 <sota_anegawa@pj100.biz>
2022-09-15 10:51:06 +0900

    ✨elif.pyの追加

---
+++ elif.py
@@ -0,0 +1,9 @@
+sales = 80
+if sales >= 100:
+    print("salesは100以上です。")
+elif sales >= 50:
+    print("salesは100未満、50以上です。")
+elif sales >= 20:
+    print("salesは50未満、20以上です。")
+else:
+    print("salesは20未満です。")
